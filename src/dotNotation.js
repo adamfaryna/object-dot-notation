382cf30c49723e9ecf6eb2abf83be3c272b28cf3
Adam Faryna <adamfaryna@gmail.com>
2017-10-24 20:21:02 +0100

    Fixed licence metadata.

--- src/dotNotation.js
+++ src/dotNotation.js
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2017 Adam Faryna <adamfaryna@appdy.net>
  *
- * Distributed under terms of the BSD license.
+ * Distributed under terms of the BSD 2-Clause license.
  */
 
 const isObject = object => Object.prototype.toString.call(object).endsWith('Object]');
